4b8587e11eec95864b827d9d9f2cc97f98984005
jeffchang <systexjeffchang@gmail.com>
2018-09-16 19:53:25 +0800

    Refactory

--- simpleCalendar.js
+++ simpleCalendar.js
@@ -1,134 +1,168 @@
-var nowid = "R1C0";
-var rnum = 1;
-var cnum = 0;
-var todayid = "";
+var nowId = "R1C0";
+var rowNum = 1;
+var columnNum = 0;
+var todayId = "";
 var nowYearMonth;
 var today = new Date();
-var firstDrawDiv = 0;
+var isFirstDrawDiv = 0;
 var preMonthCountDay;
 var preDateNum = 0;
-var isFinishPreFillup = 0;
-var grid = 0;
+var isFinishPreFillUp = 0;
+var gridNum = 0;
+
 $(function () {
-    setCalendar(new Date());
-    $("#R0C0").css("background-color", "red");
-    $('#preYear').bind("click", function () {
-        setCalendar(new Date((nowYearMonth.getFullYear() - 1) + "/" + (nowYearMonth.getMonth() + 1) + "/1"));
-    });
-    $('#preMonth').bind("click", function () {
-        nowYearMonth.getMonth() == 0 ? setCalendar(new Date((nowYearMonth.getFullYear() - 1) + "/12/1")) : setCalendar(new Date(nowYearMonth.getFullYear() + "/" + (nowYearMonth.getMonth() + 1 - 1) + "/1"));
-    });
-    $('#nextMonth').bind("click", function () {
-        nowYearMonth.getMonth() == 11 ? setCalendar(new Date((nowYearMonth.getFullYear() + 1) + "/1/1")) : setCalendar(new Date(nowYearMonth.getFullYear() + "/" + (nowYearMonth.getMonth() + 1 + 1) + "/1"));
-    });
-    $('#nextYear').bind("click", function () {
-        setCalendar(new Date((nowYearMonth.getFullYear() + 1) + "/" + (nowYearMonth.getMonth() + 1) + "/1"));
-    });
-    $('#todayMonth').bind("click", function () {
-        setCalendar(new Date());
-    });
-    $(window).keydown(function (event) {
-        switch (event.keyCode) {
-            case 38:
-                setColor("up");
-                break;
-            case 40:
-                setColor("down");
-                break;
-            case 37:
-                setColor("left");
-                break;
-            case 39:
-                setColor("right");
-                break;
-        }
-    });
+    BindButtons();
+    SetCalendar(new Date());
+    SetArrowKeys();
 });
 
-function setColor(type) {
-    $("#" + nowid).css("background-color", "white");
-    switch (type) {
-        case "up":
-            rnum - 1 < 1 ? rnum = 1 : rnum -= 1;
-            break;
-        case "down":
-            rnum + 1 > 6 ? rnum = 6 : rnum += 1;
-            break;
-        case "left":
-            cnum - 1 < 0 ? cnum = 0 : cnum -= 1;
-            break;
-        case "right":
-            cnum + 1 > 6 ? cnum = 6 : cnum += 1;
-            break;
-    }
-    nowid = "R" + rnum + "C" + cnum;
-    if (nowYearMonth.getFullYear() + "/" + (nowYearMonth.getMonth() + 1) + "/1" == today.getFullYear() + "/" + (today.getMonth() + 1) + "/1") {
-        $('#' + todayid).css("background-color", "green");
-    }
-    $("#" + nowid).css("background-color", "red");
-};
+function BindButtons() {
 
-function fillUpPreMonthDate(preNeedFillDateNum, preMonthCountDay) {
-    for (let k = 0; k <= preNeedFillDateNum; k++) {
-        $("#R1C" + k).text(preMonthCountDay - preNeedFillDateNum + k + 1).css("color", "grey").css("text-align", "center").css("background-color", "white");
+    $("#R0C0").css("background-color", "red");
+    $('#preYear').bind("click", function () {
+        SetCalendar(new Date(GetYearMonthStr(nowYearMonth, "YY", -1)));
+    });
+    $('#preMonth').bind("click", function () {
+        SetCalendar(new Date(GetYearMonthStr(nowYearMonth, "MM", 0)));
+    });
+    $('#nextMonth').bind("click", function () {
 
-    }
-    isFinishPreFillup = 1;
+        SetCalendar(new Date(GetYearMonthStr(nowYearMonth, "MM", 2)));
+    });
+    $('#nextYear').bind("click", function () {
+        SetCalendar(new Date(GetYearMonthStr(nowYearMonth, "YY", 1)));
+    });
+    $('#todayMonth').bind("click", function () {
+        SetCalendar(new Date());
+    });
+
 }
 
-function setCalendar(setDate) {
-    nowYearMonth = setDate;
-    isFinishPreFillup = 0;
-    grid = 0;
+function SetArrowKeys() {
+    $(this).keydown(function (e) {
+        switch (e.keyCode) {
+            case 38:
+                SetColor("up");
+                break;
+            case 40:
+                SetColor("down");
+                break;
+            case 37:
+                SetColor("left");
+                break;
+            case 39:
+                SetColor("right");
+                break;
+        }
+    })
+}
+
+function GetYearMonthStr(_date, _yearOrMonth, _num, _day = 1) {
+
+    switch (_yearOrMonth) {
+        case "YY":
+            return (_date.getFullYear() + _num) + "/" + (_date.getMonth() + 1) + "/" + _day
+        case "MM":
+            if ((_date.getMonth() + _num) > 12) {
+                return _date.getFullYear() + Math.floor((_date.getMonth() + _num) / 12) + "/" + ((_date.getMonth() + _num) % 12) + "/" + _day
+            }
+            else if ((_date.getMonth() + _num) < 1) {
+                return _date.getFullYear() - Math.ceil((Math.abs(_num) - _date.getMonth() + 1) / 12) + "/" + ((_date.getMonth() - Math.abs(_num)) % 12 + 12) + "/" + _day
+            }
+            else {
+                return _date.getFullYear() + "/" + (_date.getMonth() + _num) + "/" + _day
+            }
+        default:
+            break;
+    }
+
+}
+
+function SetColor(_type) {
+    $("#" + nowId).css("background-color", "white");
+    switch (_type) {
+        case "up":
+            rowNum - 1 < 1 ? rowNum = 1 : rowNum -= 1;
+            break;
+        case "down":
+            rowNum + 1 > 6 ? rowNum = 6 : rowNum += 1;
+            break;
+        case "left":
+            columnNum - 1 < 0 ? columnNum = 0 : columnNum -= 1;
+            break;
+        case "right":
+            columnNum + 1 > 6 ? columnNum = 6 : columnNum += 1;
+            break;
+    }
+    nowId = "R" + rowNum + "C" + columnNum;
+    if (GetYearMonthStr(nowYearMonth, "MM", 1) == GetYearMonthStr(today, "MM", 1)) {
+        $('#' + todayId).css("background-color", "green");
+    }
+    $("#" + nowId).css("background-color", "red");
+};
+
+function FillUpPreMonthDate(_preNeedFillDateNum, _preMonthCountDay) {
+    for (let k = 0; k <= _preNeedFillDateNum; k++) {
+        $("#R1C" + k).text(_preMonthCountDay - _preNeedFillDateNum + k + 1).css("color", "grey").css("text-align", "center").css("background-color", "white");
+
+    }
+    isFinishPreFillUp = 1;
+}
+
+function SetCalendar(_setDate) {
+    nowYearMonth = _setDate;
+    isFinishPreFillUp = 0;
+    gridNum = 0;
     preMonthCountDay = new Date(nowYearMonth.getFullYear(), nowYearMonth.getMonth(), 0).getDate();
-    var MonthCountDay = new Date(nowYearMonth.getFullYear(), nowYearMonth.getMonth() + 1, 0).getDate();
-    var FirstDayWeek = new Date(nowYearMonth.getFullYear() + "/" + (nowYearMonth.getMonth() + 1) + "/1").getDay()
-    var flagStart = 0;
-    var datenum = 1;
-    var preNeedFillDateNum = 0;
-    var nextNeedFillDateNum = 0;
+    let _monthCountDay = new Date(nowYearMonth.getFullYear(), nowYearMonth.getMonth() + 1, 0).getDate();
+    let _firstDayOfMonth = new Date(GetYearMonthStr(nowYearMonth, "MM", 1)).getDay()
+    let _gridStartNum = 0;
+    let _dateNum = 1;
+    let _preNeedFillDateCount = 0;
+    let _nextNeedFillDateCount = 0;
     $('#CaptionYear').text(nowYearMonth.getFullYear() + "/" + (nowYearMonth.getMonth() + 1));
     for (let i = 1; i <= 6; i++) {
-        firstDrawDiv == 0 ? $('#csstable').last().append("<div  class='csstr'></div>") : null;
+        isFirstDrawDiv == 0 ? $('#csstable').last().append("<div  class='csstr'></div>") : null;
         for (let j = 0; j <= 6; j++) {
-            firstDrawDiv == 0 ? $('#csstable .csstr').last().append("<div class='csstd' id='R" + i + "C" + j + "' ></div>") : null;
-            if (i == 1 && j == FirstDayWeek && isFinishPreFillup == 0) {
-                flagStart = j * i;
-                fillUpPreMonthDate(preNeedFillDateNum, preMonthCountDay);
+            isFirstDrawDiv == 0 ? $('#csstable .csstr').last().append("<div class='csstd' id='R" + i + "C" + j + "' ></div>") : null;
+            if (i == 1 && j == _firstDayOfMonth && isFinishPreFillUp == 0) {
+                _gridStartNum = j * i;
+                FillUpPreMonthDate(_preNeedFillDateCount, preMonthCountDay);
             }
-            if (isFinishPreFillup != 1) {
-                preNeedFillDateNum++;
+            if (isFinishPreFillUp != 1) {
+                _preNeedFillDateCount++;
             }
-            if (datenum <= MonthCountDay) {
+            if (_dateNum <= _monthCountDay) {
 
-                if (i == 1 && preNeedFillDateNum - grid == 0) {
-                    datenum = 1;
+                if (i == 1 && _preNeedFillDateCount - gridNum == 0) {
+                    _dateNum = 1;
                 }
                 //first day is sunday
-                if (grid == 0 && preNeedFillDateNum == 0) {
-                    fillUpPreMonthDate(7, preMonthCountDay);
-                    datenum = 0;
+                if (gridNum == 0 && _preNeedFillDateCount == 0) {
+                    FillUpPreMonthDate(7, preMonthCountDay);
+                    _dateNum = 0;
                     i++;
                     j--;
                 }
                 else {
-                    $("#R" + i + "C" + j).text(datenum).css("text-align", "center").css("background-color", "white").css("color", "black");
+                    $("#R" + i + "C" + j).text(_dateNum).css("text-align", "center").css("background-color", "white").css("color", "black");
                 }
-                datenum++;
-                grid++;
+                _dateNum++;
+                gridNum++;
             }
             else {
-                nextNeedFillDateNum++
+                _nextNeedFillDateCount++
                 //fill Up Next Month Date
-                $("#R" + i + "C" + j).text(nextNeedFillDateNum).css("color", "grey").css("text-align", "center");
+                $("#R" + i + "C" + j).text(_nextNeedFillDateCount).css("color", "grey").css("text-align", "center");
             }
-            if (nowYearMonth.getFullYear() + "/" + (nowYearMonth.getMonth() + 1) + "/" + (datenum - 1) == today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate()) {
+            if (
+                GetYearMonthStr(nowYearMonth, "MM", 1, (_dateNum - 1)) == GetYearMonthStr(today, "MM", 1, today.getDate())) {
                 $("#R" + i + "C" + j).css("background-color", "green");
-                todayid = "R" + i + "C" + j;
+                todayId = "R" + i + "C" + j;
             }
-            flagStart == 7 ? flagStart = i * 7 : null;
+            _gridStartNum == 7 ? _gridStartNum = i * 7 : null;
         }
     }
-    firstDrawDiv = 1;
-    $("#" + nowid).css("background-color", "red");
+    isFirstDrawDiv = 1;
+    $("#" + nowId).css("background-color", "red");
 };
